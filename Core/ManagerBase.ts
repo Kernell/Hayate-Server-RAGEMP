cd7711fe94b034f92c90c4b825406507800ff019
Kernell <kernell.it@gmail.com>
2017-05-27 09:50:15 +0300

    [Core] Added ManagerBase for managers

---
+++ Core/ManagerBase.ts
@@ -0,0 +1,65 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+import Server from "../Server";
+
+export default class ManagerBase
+{
+	protected List;
+
+	constructor( server : Server )
+	{
+		server.RegisterManager( this );
+		
+		this.List = {};
+	}
+	
+	public DestroyAll() : void
+	{
+		if( this.List )
+		{
+			for( let object of this.List )
+			{
+				object.Destroy();
+			}
+		}
+	}
+	
+	public AddToList( object ) : void
+	{
+		this.List[ object.GetID() ] = object;
+	}
+	
+	public RemoveFromList( object ) : void
+	{
+		this.List[ object.GetID() ] = null;
+	}
+
+	public Get( id : number ) : void
+	{
+		return this.List[ id ];
+	}
+	
+	public GetAll()
+	{
+		return this.List;
+	}
+
+	public Init() : Boolean
+	{
+		return true;
+	}
+
+	public DoPulse( date : Date ) : void
+	{
+	}
+}

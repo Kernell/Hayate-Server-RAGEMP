e0f828707276afc1dd5fb7bb0af392bf7a5e1735
Kernell <kernell.it@gmail.com>
2017-05-27 09:49:51 +0300

    Fixed constructor.name TS error (Undefined property)

---
+++ Types/Function.d.ts
@@ -0,0 +1,16 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+interface Function
+{
+	name: string;
+}

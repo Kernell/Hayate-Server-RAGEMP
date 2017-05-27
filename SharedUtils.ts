ea0fe0b9bbb1ef9fe7ad9069d622594c60df5900
Kernell <kernell.it@gmail.com>
2017-05-27 09:49:00 +0300

    [SharedUtils] Added String::pad method for padding

---
+++ SharedUtils.ts
@@ -0,0 +1,13 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+import "./SharedUtils/String";
---
+++ SharedUtils/String.ts
@@ -0,0 +1,33 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+interface String
+{
+	pad( num : number, char ?: string, left ?: boolean ) : string;
+}
+
+String.prototype.pad = function( num : number, char : string = ' ', left : boolean = false ) : string
+{
+	let spaces = "";
+
+	for( let i = this.length; i < num; ++i )
+	{
+		spaces += " ";
+	}
+
+	if( left )
+	{
+		return spaces + this;
+	}
+
+	return this + spaces;
+}

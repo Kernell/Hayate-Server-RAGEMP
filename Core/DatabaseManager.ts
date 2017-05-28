d0cc16fcab98206fc437880e8f3cd401ba084e3b
Kernell <kernell.it@gmail.com>
2017-05-28 11:07:33 +0300

    Added database manager with TypeORM integration

---
+++ Core/DatabaseManager.ts
@@ -0,0 +1,83 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+import * as ORM       from "typeorm";
+import Vehicle        from "../Entity/Vehicle";
+import Console        from "../Entity/Console";
+import Server         from "../Server";
+import ManagerBase    from "./ManagerBase";
+
+export default class DatabaseManager extends ManagerBase
+{
+	private Hostname : string;
+	private Username : string;
+	private Password : string;
+	private Database : string;
+
+	private Connection : ORM.Connection;
+
+	constructor( server : Server )
+	{
+		super( server );
+
+		this.Connection = null;
+
+		this.Hostname = "127.0.0.1";
+		this.Username = "hayate_app";
+		this.Password = "W4WOHDfMoX2d2VYp";
+		this.Database = "hayate_app";
+	}
+
+	public Init() : Promise< any >
+	{
+		if( this.Connection == null )
+		{
+			return ORM.createConnection(
+				{
+					driver:
+					{
+						type       : "mysql",
+						port       : 3306,
+						host       : this.Hostname,
+						username   : this.Username,
+						password   : this.Password,
+						database   : this.Database
+					},
+					entities       :
+					[
+						Vehicle,
+					],
+					autoSchemaSync : true,
+				}
+			).then(
+				( connection ) =>
+				{
+					this.Connection = connection;
+				}
+			).catch(
+				( error ) =>
+				{
+					Console.WriteLine( "MySQL: %s", error );
+
+					throw new Error( error );
+				}
+			);
+		}
+
+		return super.Init();
+	}
+
+	public GetRepository< Entity >( entityClassOrName : ORM.ObjectType< Entity > | string ) : ORM.Repository< Entity >
+	{
+		return this.Connection.getRepository( entityClassOrName );
+	}
+}

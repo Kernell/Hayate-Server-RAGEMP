acb3c3d28b4e38d058c0e210855bd350972f76b5
Dmitry Korolev <kernell.it@gmail.com>
2017-05-29 15:17:59 +0300

    Added Vehicle entity class with database integration (ORM). Added base manager for the vehicles

---
+++ Core/VehicleManager.ts
@@ -0,0 +1,37 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+import Console          from "../Entity/Console";
+import Server           from "../Server";
+import ManagerBase      from "./ManagerBase";
+import DatabaseManager  from "./DatabaseManager";
+import Vehicle          from "../Entity/Vehicle";
+
+
+export default class VehicleManager extends ManagerBase< Vehicle >
+{
+	constructor( server : Server )
+	{
+		super( server );
+
+		this.Dependency = server.DatabaseManager;
+	}
+
+	public Init() : Promise< any >
+	{
+		return super.Init().then(
+			() =>
+			{
+			}
+		);
+	}
+}
---
+++ Entity/Vehicle.ts
@@ -0,0 +1,76 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+import * as ORM       from "typeorm";
+
+import Entity from "./Entity";
+
+@ORM.Entity( "vehicles" )
+export default class Vehicle extends Entity
+{
+	@ORM.PrimaryGeneratedColumn()
+	protected id  : number;
+
+	@ORM.Column( "int" )
+	protected model : number;
+
+	@ORM.Column()
+	protected position : string;
+
+	@ORM.Column()
+	protected rotation : string;
+
+	@ORM.Column( "int" )
+	protected dimension : number;
+
+	@ORM.Column()
+	protected color : string;
+
+	@ORM.Column()
+	protected plate : string;
+
+	@ORM.Column( { name: "default_position" } )
+	protected defaultPosition : string;
+
+	@ORM.Column( { name: "default_rotation" } )
+	protected defaultRotation : string;
+
+	@ORM.Column( { name: "default_interior", type: "int" } )
+	protected defaultInterior : number;
+
+	@ORM.Column( { name: "default_dimension", type: "int" } )
+	protected defaultDimension : number;
+
+	@ORM.CreateDateColumn( { name: "created_at" } )
+	protected createdAt : string;
+
+	@ORM.Column( { type: "datetime", name: "deleted_at", nullable: true, default: null } )
+	protected deletedAt : string;
+
+	constructor( model, position, rotation, dimension, color, plate )
+	{
+		let id     = 0;
+		let entity = mp.vehicles.new( 123, position );
+
+		super( id, entity );
+	}
+
+	public GetPlate() : string
+	{
+		return this.plate;
+	}
+
+	public GetColor() : any
+	{
+		return this.color;
+	}
+}

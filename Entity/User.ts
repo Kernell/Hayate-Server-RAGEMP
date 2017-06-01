5adec10dbc61411bea29587de705090720dfb43e
Dmitry Korolev <kernell.it@gmail.com>
2017-06-01 13:15:48 +0300

    [User] Base structure of the entity. Added password static methods

--- Entity/User.ts
+++ Entity/User.ts
@@ -10,7 +10,8 @@
 *
 *********************************************************/
 
-import * as ORM from "typeorm";
+import * as ORM    from "typeorm";
+import * as Crypto from "crypto";
 
 import { Character } from "./Character";
 
@@ -23,22 +24,138 @@ export class User
 	@ORM.Column()
 	protected name : string;
 
-	@ORM.OneToMany( type => Character, character => character.user )
-    public characters : Character[] = [];
+	@ORM.Column()
+	protected email : string;
+
+	@ORM.Column()
+	protected password : string;
+
+	@ORM.Column()
+	protected salt : string;
+	
+	@ORM.CreateDateColumn( { name: "loggedin_at" } )
+	protected loggedInAt : Date;
+
+	@ORM.CreateDateColumn( { name: "loggedout_at" } )
+	protected loggedOutAt : Date;
 
 	@ORM.CreateDateColumn( { name: "created_at" } )
-	protected createdAt : string;
+	protected createdAt : Date;
 
 	@ORM.Column( { type: "datetime", name: "deleted_at", nullable: true, default: null } )
-	protected deletedAt : string;
+	protected deletedAt : Date;
+
+	@ORM.OneToMany( type => Character, character => character.user )
+    public characters : Character[] = [];
 
 	public GetID() : number
 	{
 		return this.id;
 	}
 
+	public GetName() : string
+	{
+		return this.name;
+	}
+
+	public SetName( name : string ) : void
+	{
+		this.name = name;
+	}
+
+	public GetEmail() : string
+	{
+		return this.email;
+	}
+
+	public GetLoggedInDate() : Date
+	{
+		return this.loggedInAt;
+	}
+
+	public UpdateLoggedInDate() : void
+	{
+		this.loggedInAt = new Date();
+	}
+
+	public GetLoggedOutDate() : Date
+	{
+		return this.loggedOutAt;
+	}
+
+	public UpdateLoggedOutDate() : void
+	{
+		this.loggedInAt = new Date();
+	}
+
+	public GetCreatedDate() : Date
+	{
+		return this.createdAt
+	}
+
 	public GetCharacters() : Character[]
 	{
 		return this.characters;
+	}
+
+	public static GeneratePassword( length : number = 12, specialChars : boolean = false, extraSpecialChars : boolean = false ) : string
+	{
+		let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+		if( specialChars )
+		{
+			chars += "!@#$%^&*()";
+		}
+			
+		if( extraSpecialChars )
+		{
+			chars = chars + "-_ []{}<>~`+=,.;:/?|";
+		}
+
+		let password = "";
+			
+		for( let i = 1; i < length; ++i )
+		{
+			password += chars.charAt( Math.floor( Math.random() * chars.length ) );
+		}
+
+		return password;
+	}
+
+	public static GenerateSalt( length : number = 16 ) : string
+	{
+		let salt = "";
+			
+		for( let i = 1; i < length; ++i )
+		{
+			let rand = Math.floor( Math.random() * 127 ) + 33;
+
+			salt += String.fromCharCode( rand );
+		}
+			
+		return salt;
+	}
+
+	public static HashPassword( password : string, salt : string = null ) : string
+	{
+		if( salt == null )
+		{
+			salt = this.GenerateSalt();
+		}
+
+		let passHash = Crypto.createHash( "sha512" ).update( password ).digest( "hex" );
+		let saltHash = Crypto.createHash( "sha512" ).update( salt ).digest( "hex" );
+
+		return Crypto.createHash( "sha256" ).update( passHash + saltHash ).digest( "hex" );
+	}
+
+	public static CheckPassword( password : string, passwordHashed : string, salt : string ) : boolean
+	{
+		if( password.length == 0 || passwordHashed.length < 64 || salt.length == 0 )
+		{
+			return false;
+		}
+			
+		return this.HashPassword( password, salt ) == passwordHashed;
 	}
 }

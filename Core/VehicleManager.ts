a0ed47996b89f05ce47d08dded6e34e80fe67a7d
Dmitry Korolev <kernell.it@gmail.com>
2017-06-02 18:48:01 +0300

    Test code (temp)

--- Core/VehicleManager.ts
+++ Core/VehicleManager.ts
@@ -10,6 +10,7 @@
 *
 *********************************************************/
 
+import * as ORM         from "typeorm";
 import { Console }      from "../Entity/Console";
 import { Vehicle }      from "../Entity/Vehicle";
 import Server           from "../Server";
@@ -18,6 +19,8 @@ import DatabaseManager  from "./DatabaseManager";
 
 export default class VehicleManager extends ManagerBase< Vehicle >
 {
+	private Repository : ORM.Repository< Vehicle > = null;
+
 	constructor( server : Server )
 	{
 		super( server );
@@ -28,8 +31,34 @@ export default class VehicleManager extends ManagerBase< Vehicle >
 	public Init() : Promise< any >
 	{
 		return super.Init().then(
-			() =>
+			async () =>
 			{
+				this.Repository = ( this.Dependency as DatabaseManager ).GetRepository( Vehicle );
+
+				try
+				{
+					let vehicle = new Vehicle( VehicleModel.Elegy2, new Vector3( 192, 168, 0 ), new Vector3(), 0, new Color( 255, 0, 0 ), 'Lorem' )
+
+					Console.WriteLine( "Elegy2: 0x%X, %s", vehicle.GetModel(), vehicle.GetPosition() );
+
+					await this.Repository.persist( vehicle );
+
+					let vehicles = await this.Repository.find();
+
+					for( let vehicle of vehicles )
+					{
+						//let id       = vehicle.GetID();
+						//let model    = vehicle.GetModel();
+						//let position = vehicle.GetPosition();
+						//let color    = vehicle.GetColor();
+
+						//Console.WriteLine( "Vehicle ID: %d | model: %d | position: %d, %d, %d | color: %d, %d, %d", id, model, position.x, position.y, position.z, color.Red, color.Green, color.Blue );
+					}
+				}
+				catch( e )
+				{
+					console.log( e );
+				}
 			}
 		);
 	}

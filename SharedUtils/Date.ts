8ec63f5a605c9f5e69823869f9ffe63ec0933cb9
Dmitry Korolev <kernell.it@gmail.com>
2017-06-02 18:35:21 +0300

    Fix date time

---
+++ SharedUtils/Date.ts
@@ -0,0 +1,34 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+Date.prototype.toISOString = function()
+{
+	let pad = ( number ) =>
+	{
+		if( number < 10 )
+		{
+			return '0' + number;
+		}
+
+		return number;
+    }
+
+	let y = this.getFullYear();
+	let m = pad( this.getMonth() + 1 );
+	let d = pad( this.getDate() );
+
+	let h = pad( this.getHours() );
+	let i = pad( this.getMinutes() );
+	let s = pad( this.getSeconds() );
+
+	return `${y}-${m}-${d} ${h}:${i}:${s}`;
+}

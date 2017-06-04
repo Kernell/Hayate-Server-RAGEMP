94178b778e24cd83bec3100a325933d1ad125105
Kernell <kernell.it@gmail.com>
2017-06-04 23:47:24 +0300

    Missed file

---
+++ Types/VehicleColor.ts
@@ -0,0 +1,22 @@
+﻿/*********************************************************
+*
+*  Copyright © 2017, Raybit Games.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+class VehicleColor
+{
+	public constructor( color1 ?: Color, color2 ?: Color )
+	{
+		this[ 0 ] = color1 || new Color();
+		this[ 1 ] = color2 || new Color();
+	}
+}
+
+module.exports = VehicleColor;

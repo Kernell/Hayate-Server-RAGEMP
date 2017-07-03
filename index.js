12f341bd4a4ba8f89e0b0c92eee92e5fff959c46
Dmitry Korolev <kernell.it@gmail.com>
2017-07-03 15:56:48 +0300

    Workaround for typescript path mapping

--- index.js
+++ index.js
@@ -20,6 +20,49 @@ global.printf = require( "printf" );
 const FileSystem = require( 'fs' );
 const Path       = require( 'path' );
 
+(function()
+{
+	const CH_PERIOD   = 46;
+	const baseUrl     = __dirname + '/bin/';
+	const existsCache = {};
+
+	const moduleProto = Object.getPrototypeOf( module );
+	const origRequire = moduleProto.require;
+
+	moduleProto.require = function( request )
+	{
+		let existsPath = existsCache[ request ];
+
+		if( existsPath === undefined )
+		{
+			existsPath = '';
+
+			if( !path.isAbsolute( request ) && request.charCodeAt( 0 ) !== CH_PERIOD )
+			{
+				const ext = path.extname( request );
+				const basedRequest = path.join( baseUrl, ext ? request : request + '.js' );
+				
+				if( fs.existsSync( basedRequest ) )
+				{
+					existsPath = basedRequest;
+				}
+				else
+				{
+					const basedIndexRequest = path.join( baseUrl, request, 'index.js' );
+
+					existsPath = fs.existsSync( basedIndexRequest ) ? basedIndexRequest : '';
+				}
+			}
+
+			existsCache[ request ] = existsPath
+		}
+
+		return origRequire.call( this, existsPath || request );
+	}
+}
+)();
+
+
 let include = ( file ) =>
 {
 	let _export = require( './bin/' + file );
